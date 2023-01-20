a85533f3b465c47097d3b32f092aa88673406545
umidkhan <umidxondeveloper@mail.ru>
2023-01-20 14:23:58 +0500

    fix

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Display from "./Display/Display";
+import ButtonPanel from "./ButtonPanel/ButtonPanel";
+import calculate from "../../src/logic/calculate";
+import "./App.css";
+
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = (buttonName) => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
